7bf4de6a1ad0e554da905e18ff7fc0d373780646
unknown <binghao9312>
2026-09-22 15:15:51 +0800

    Update uart_tx: correct start/stop bit levels and bit index boundary

--- src/uart_tx.v
+++ src/uart_tx.v
@@ -26,7 +26,7 @@ always @(*)begin
         IDLE:       NS = (i_Tx_valid)? START : IDLE; 
         START:      NS = READ;
         READ:       NS = OUTPUT;
-        OUTPUT:     NS = (bit_idx == DATA_BITS)? END : OUTPUT;
+        OUTPUT:     NS = (bit_idx == DATA_BITS - 1)? END : OUTPUT;
         END:        NS = IDLE;
         default:    NS = IDLE;
     endcase
@@ -56,13 +56,14 @@ always @(posedge i_clk or posedge i_rst)begin
     else begin
         if(bit_idx == DATA_BITS) begin
             bit_idx     <= 0;
-            o_stx       <= 1;
         end 
-        else if(CS == START) o_stx <= 1;
-        else if(CS == END)   o_stx <= 0;
         else if(i_bps_en)begin
-            o_stx       <= data_tmp[bit_idx];
-            bit_idx     <= bit_idx + 1;
+            if(CS == START)         o_stx <= 0;
+            else if(CS == END)      o_stx <= 1;
+            else begin
+                o_stx       <=      data_tmp[bit_idx];
+                bit_idx     <=      bit_idx + 1;
+            end
         end
         else begin
             bit_idx     <= bit_idx;
